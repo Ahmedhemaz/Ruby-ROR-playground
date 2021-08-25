b032371ad564784b1ca670ae4534eaf650a5b8f8
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-25 18:00:58 +0200

    file read and write

--- ch1/temp_converter.rb
+++ ch1/temp_converter.rb
@@ -2,3 +2,13 @@ print 'Enter the temp in Celsius: '
 celsius = gets
 fahrenheit = (celsius.to_i * 9 / 5) + 32
 puts "The result is: #{fahrenheit}"
+
+puts 'FROM FILE'
+celsius = File.read('temp.dat')
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+puts "The result is: #{fahrenheit}"
+
+celsius = 30
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+fh = File.new('temp.out', 'w')
+fh.puts(fahrenheit)
