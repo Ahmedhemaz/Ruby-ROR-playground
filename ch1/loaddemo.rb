ce5fb7f937f557344d8970745ccfd64205ca17b0
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-26 09:41:08 +0200

    require ruby files

--- ch1/loaddemo.rb
+++ ch1/loaddemo.rb
@@ -2,3 +2,8 @@ puts 'This is the first (master) program file.'
 load 'loadee.rb' # if not found will look for it in $LOAD_PATH
 load './sub-ch1/loadee2.rb' # if not found will look for it in $LOAD_PATH
 puts 'And back again to the first file.'
+
+puts 'This is with require'
+require './loadee' # if not found will look for it in $LOAD_PATH
+require './sub-ch1/loadee2' # if not found will look for it in $LOAD_PATH
+puts 'And back again to the first file.'
