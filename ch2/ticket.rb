4716a9a5f3684e58d7f03bb1eae7826b6eabcc6c
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-26 14:26:23 +0200

    falsy and truthy values test

--- ch2/ticket.rb
+++ ch2/ticket.rb
@@ -28,3 +28,59 @@ puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" \
      "The performer is #{ticket.performer}.\n" \
      "The seat is #{ticket.seat}, " \
      "and it costs $ #{ticket.price}"
+
+puts '==========================='
+def ticket.availability_status
+  'sold'
+end
+
+def ticket.available?
+  false
+end
+
+if ticket.available?
+  puts 'Ya Wala'
+else
+  puts 'Ya Booma'
+end
+puts '==========================='
+
+# falsy and truthy values test
+IS_TRUTHY = 'is truthy'.freeze
+IS_FALSY = 'is falsy'.freeze
+EMPTY_STRING = 'empty string'.freeze
+EXAMPLE_STRING = 'EXAMPLE string'.freeze
+ZERO_NUMBER = 'NUMBER ZERO'.freeze
+NEGATIVE_NUMBER = 'NEGATIVE NUMBER'.freeze
+EXAMPLE_NUMBER = 'EXAMPLE NUMBER'.freeze
+NIL_VALUE = 'NIL VALUE'.freeze
+
+if ''
+  puts EMPTY_STRING + IS_TRUTHY
+else
+  puts EMPTY_STRING + IS_FALSY
+end
+
+if 'abc'
+  puts EXAMPLE_STRING + IS_TRUTHY
+else
+  puts EXAMPLE_STRING + IS_FALSY
+end
+
+if 0
+  puts ZERO_NUMBER + IS_TRUTHY
+else
+  puts ZERO_NUMBER + IS_FALSY
+end
+
+if -1
+  puts NEGATIVE_NUMBER + IS_TRUTHY
+else
+  puts NEGATIVE_NUMBER + IS_FALSY
+end
+
+if nil
+  puts NIL_VALUE + IS_TRUTHY
+else
+  puts NIL_VALUE + IS_FALSY
+end
