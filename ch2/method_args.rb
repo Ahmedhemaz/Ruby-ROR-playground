5b4f78777ef2395d37566421ba217f8a652653e8
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-29 09:01:02 +0200

    Spnge args

--- ch2/method_args.rb
+++ ch2/method_args.rb
@@ -9,6 +9,13 @@ end
 obj.multi_args
 obj.multi_args 1, 2, 3
 
+
+
+puts '==============================Sponge args==============================='
+# syntax error as optional can't be to the right of sponge args
+def broken_args(x,*y,z=1)
+end
+
 puts '==============================one arg exception==============================='
 def obj.one_arg(_x)
   puts 'I require one and only one argument!'
