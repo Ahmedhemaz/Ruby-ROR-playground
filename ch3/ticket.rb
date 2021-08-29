f08de93b86c4afc49022c1631149f5c5bc291cb2
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-29 11:51:15 +0200

    attr_* ticket.rb

--- ch3/ticket.rb
+++ ch3/ticket.rb
@@ -1,11 +1,13 @@
 # modeling Ticket
 class Ticket
+  attr_accessor :price
   attr_reader :venue, :date
 
-  def initialize(venue, date)
+  def initialize(venue, date, price)
     puts 'Ticket initializer'
     @venue = venue
     @date = date
+    @price = price
   end
 
   def event
@@ -13,10 +15,13 @@ class Ticket
   end
 end
 
-obj = Ticket.new('Ruby', '29/8/2021')
+obj = Ticket.new('Ruby', '29/8/2021', 30)
 obj.event
 puts obj.venue
 puts obj.date
+puts obj.price
+obj.price = 45
+puts obj.price
 
 puts '=====================Over riding methods========================'
 # Over riding class methods
