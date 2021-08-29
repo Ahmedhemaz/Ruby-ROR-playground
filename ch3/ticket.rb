4344c5a3592fa61c7694056621dd885a624ed682
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-29 14:04:38 +0200

    adding method to class Ticket

--- ch3/ticket.rb
+++ ch3/ticket.rb
@@ -3,12 +3,16 @@ class Ticket
   attr_accessor :price
   attr_reader :venue, :date
 
-  def initialize(venue, date, price)
+  def initialize(venue, date, price = 0)
     puts 'Ticket initializer'
     @venue = venue
     @date = date
     @price = price
   end
+
+  #   def self.most_expensive(*tickets)
+  #     tickets.max_by(&:price)
+  #   end
 
   def event
     puts "Can't really be specified yet..."
@@ -46,3 +50,18 @@ class C
   end
 end
 obj.q  # will print the new added q method
+
+puts '=================max method adding methods to the class Ticket as it\'s an object of Class ==========================='
+
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new('Town Hall', '2013-11-12')
+cc = Ticket.new('Convention Center', '2014-12-13')
+fg = Ticket.new('Fairgrounds', '2015-02-14')
+th.price = 12.55
+cc.price = 10.00
+fg.price = 18.00
+highest = Ticket.most_expensive(th, cc, fg)
+puts "The highest-priced ticket is the one for #{highest.venue}."
