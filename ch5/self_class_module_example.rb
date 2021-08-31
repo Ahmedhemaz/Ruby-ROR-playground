9303dc17fb32fdcd3fb80ab6b2c51d654ad80321
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-08-31 11:33:25 +0200

    class-module-self

---
+++ ch5/self_class_module_example.rb
@@ -0,0 +1,12 @@
+class C
+  puts 'Class block....'
+  puts "self is #{self}"
+
+  module M
+    puts 'Module block....'
+    puts "self is #{self}"
+  end
+
+  puts 'Class block....'
+  puts "self is #{self}"
+end
