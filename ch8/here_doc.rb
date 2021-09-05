b514b12316b715e738f3897784ef767539e17b0c
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-05 15:13:01 +0200

    here doc as array element and method arg

--- ch8/here_doc.rb
+++ ch8/here_doc.rb
@@ -14,3 +14,16 @@ end
 
 p RubyWelcomeWegon.new.message
 p RubyWelcomeWegon.new.single_quote_message
+
+array = [1, 2, 3, <<~HERE, 4]
+  YOLO
+HERE
+p array
+
+def here_docs_as_arg(a, b, c)
+  p a, b, c
+end
+
+here_docs_as_arg(1, 2, <<~HERE)
+  Ya Wala
+HERE
