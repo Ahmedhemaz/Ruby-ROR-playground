bfb7894a7d23afe518636453c464fd53fe1c272f
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-07 16:30:51 +0200

    each.with_index

--- ch9/hashes.rb
+++ ch9/hashes.rb
@@ -2,3 +2,5 @@ hash = { red: 'ruby', white: 'diamond', green: 'emerald' }
 hash.each_with_index { |(key, value), i| puts "pari #{i} is: #{key}/#{value}" }
 puts '============each===================='
 hash.each { |key, value| puts "#{key}: #{value}" }
+
+hash.each.with_index { |(key, value), i| puts "#{i} => #{key}:#{value}" }
