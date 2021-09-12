1c9a73b156fff6f38eaff305f4276b7d99da0be7
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-12 15:07:40 +0200

    visible logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,4 @@
 class Comment < ApplicationRecord
+  include Visible
   belongs_to :article
-
-  VALID_STATUSES = %w[public private archived].freez
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
